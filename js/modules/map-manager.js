c9b9c98e25546c5381f7f0f71232792dfc0513ac
diegosb <dlopeztostado09@gmail.com>
2026-04-30 10:08:07 +0200

    deploy

--- js/modules/map-manager.js
+++ js/modules/map-manager.js
@@ -4,6 +4,9 @@
 
 import L from 'leaflet';
 import 'leaflet.markercluster';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 
 import { AppState } from './store.js';
 import { icons, colors, lugaresIcons, lugaresColors } from './constants.js';
